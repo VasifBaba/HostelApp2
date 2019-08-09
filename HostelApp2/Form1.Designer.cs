822d3af402bd7e2e12107006f4e1525b4a73eb89
VasifBaba <vasifbaba3.14@gmail.com>
2019-08-09 15:44:03 +0400

    dfdf

---
+++ HostelApp2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HostelApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblmbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.pcImage = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblmbl
+            // 
+            this.lblmbl.AutoSize = true;
+            this.lblmbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmbl.Location = new System.Drawing.Point(223, 35);
+            this.lblmbl.Name = "lblmbl";
+            this.lblmbl.Size = new System.Drawing.Size(126, 46);
+            this.lblmbl.TabIndex = 0;
+            this.lblmbl.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(293, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(231, 210);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(234, 30);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.TextChanged += new System.EventHandler(this.TxtEmail_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(231, 450);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(266, 377);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(158, 37);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // pcImage
+            // 
+            this.pcImage.Location = new System.Drawing.Point(664, 198);
+            this.pcImage.Name = "pcImage";
+            this.pcImage.Size = new System.Drawing.Size(341, 294);
+            this.pcImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcImage.TabIndex = 5;
+            this.pcImage.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(231, 633);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 813);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pcImage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblmbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblmbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.PictureBox pcImage;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HostelApp2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using HostelApp2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HostelApp2
+{
+    
+    public partial class Form1 : Form
+    {
+        hostelDBEntities db = new hostelDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblmbl.Text = "Compar Hostel";
+            lblmbl.Location = new Point((this.ClientSize.Width - lblmbl.Size.Width) / 2, 20);
+            pcImage.Image = new Bitmap(@"C:\Users\User\Source\Repos\HostelApp2\HostelApp2\image\abc.png");
+            pcImage.Location = new Point((this.ClientSize.Width - pcImage.Size.Width) / 2,
+                 (this.ClientSize.Height - pcImage.Size.Height) / 2);
+        }
+
+        private void TxtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            AdminDashboard ad = new AdminDashboard();
+            ad.ShowDialog();
+        }
+    }
+}
